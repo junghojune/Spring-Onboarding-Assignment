e39bc0b303190f99e9375678b28180bc4a776db8
hosu <76641246+junghojune@users.noreply.github.com>
2024-11-10 19:04:54 +0900

    Feat : SwaggerConfig 추가

---
+++ src/main/java/com/intern/work/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.intern.work.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    private static final String BEARER_TOKEN = "Bearer";
+
+    @Bean
+    public OpenAPI openAPI() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme(BEARER_TOKEN)
+                .bearerFormat("JWT")
+        );
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Intern Onboarding API") // API의 제목
+                .description("인턴 온보딩 과제입니다.") // API에 대한 설명
+                .version("1.0.0"); // API의 버전
+    }
+}
